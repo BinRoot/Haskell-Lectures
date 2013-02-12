7a73b6ba44dc150b481b4176dca07519d4192039
BinRoot <nick722@gmail.com>
2013-02-12 12:45:32 -0500

    updated lec 5 button

--- nav.js
+++ nav.js
@@ -22,7 +22,7 @@ document.write('\
                               <li><a href="lec02.html">2 - (["lists", "tuples"], "and data types")</a></li>\
 			      <li><a href="lec03.html">3 - types :: type1 → type2</a></li>\
  			      <li><a href="lec04.html">4 - #SyntaxSwag</a></li>\
-                              <li class="disabled"><a href="#">5 - Make Your Own Data Type</a></li>\
+			      <li><a href="lec05.html">5 - Make Your Own Data Type</a></li>\
                               <li class="disabled"><a href="#">6 - Maps, Folds, and Beyond</a></li>\
                               <li class="disabled"><a href="#">7 - The Prolific Haskell Community</a></li>\
 			      <li class="disabled"><a href="#">8 - Input/Output</a></li>\
